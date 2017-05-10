080eec3600079a3fadc6f61c45374ab867278a37
hamzahaly <hzhaly93@gmail.com>
2017-05-09 21:01:37 -0700

    needs to finish

---
+++ js/Bubble.js
@@ -0,0 +1,87 @@
+var Bubble = function() {
+    //Set default values
+    var height = 700;
+    var width = 700;
+    var margin = {
+        left: 70,
+        bottom: 50,
+        top: 30,
+        right: 10
+    };
+
+    //Chart function to return
+    var chart = function(selection) {
+        var chartHeight = height - margin.bottom - margin.top;
+        var chartWdith = width - margin.left - margin.right;
+    
+    var pack = d3.pack()
+        .size([width, height])
+        .padding(1.5);
+
+        selection.each(function(data) {
+            var nestedData = data.values;
+
+            console.log(nestedData);
+
+            var root = d3.hierarchy({
+                values: nestedData
+            }, function(d) {
+                return d.values;
+            });
+
+            console.log(root);
+
+            root.sum(function(d) {
+                return +d.Calories;
+            });
+
+            // var categories = nestedData.map(function(d) {
+            //     return d.key;
+            // });
+
+            //var colorScale = d3.scaleOrdinal().range(d3.schemeCategory10);
+
+            //SVG element to draw things in
+            var svg = d3.select(this).selectAll('.chart')
+                .data([nestedData]);
+
+
+            //Setting attrs for the svg element to display viz in
+            var svgEnter = svg.enter()
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height);
+
+            //Circle nodes, g's, to append circles to.
+            var node = svgEnter.selectAll('.node').data(pack(root).leaves())
+                .enter().append('g')
+                .attr('class', 'node')
+                .attr('transform', function(d) { 
+                    console.log(d);
+                    return 'translate(' + d.x + ',' + d.y + ')';
+                })
+
+            node.append('circle')
+                .attr('r', function(d) {
+                    return d.r;
+                })
+                .attr('fill', 'blue');
+
+            node.exit().remove();
+        });
+    };
+
+    chart.height = function(value) {
+        if (!arguments.length) return height;
+        height = value;
+        return chart;
+    };
+
+    chart.width = function(value) {
+        if (!arguments.length) return width;
+        width = value;
+        return chart;
+    };
+
+    return chart;
+};
--- js/main.js
+++ js/main.js
@@ -1,29 +1,76 @@
 $(function() {
-    //Variables to show
-    var nestedData;
+    d3.csv('data/menu.csv', function(error, data) {
+    var bubble = Bubble().width(560).height(900);
 
-    d3.csv('data/country-data.csv', function(error, data) {
-        //Necessary to prep data?
+    var nestedData = d3.nest()
+        .key(function(d) {
+            return d.Category;
+        })
+        .entries(data);
 
-        //Nest data into heirarchical structure
-        nestedData = d3.nest()
-            .key(function(d) {
-                return d.region;
-            })
-            .entries(data);
-    });
+    console.log(nestedData);
 
-    var sunburst = Sunburst().width(700).height(700);
+    var charts = d3.select('#viz').selectAll('.chart')
+        .datum([nestedData]);
 
-    var draw = function() {
+    charts.enter().append('div')
+        .attr('class', 'chart')
+        .merge(charts)
+        .call(bubble);
 
-        var charts = d3.select('#viz').selectAll('.chart')
-            .data(nestedData);
-        
-        //enter
+    charts.exit().remove();
 
-        //exit
-    };
+
+    //Necessary to prep data?
+
+    //Nest data into heirarchical structure
+    // var svg = d3.select('#viz')
+    // .append('svg')
+    // .attr('width', width)
+    // .attr('height', height);
+
+
+
+    // var root = d3.hierarchy({
+    //     values: nestedData
+    // }, function(d) {
+    //     return d.values;
+    // });
+
+    // root.sum(function(d) {
+    //     return +d.Calories;
+    // })
+
+    // var categories = nestedData.map(function(d) {
+    //     return d.key;
+    // });
+
+    // var colorScale = d3.scaleOrdinal().domain(categories).range(d3.schemeCategory10);
+
+    // var pack = d3.pack()
+    //     .size([width, height])
+    //     .padding(1.5);
+
+    // var node = svg.selectAll('.node').data(pack(root).leaves())
+    //     .enter().append('g')
+    //     .attr('class', 'node')
+    //     .attr('transform', function(d) { 
+    //         console.log(d);
+    //         return 'translate(' + d.x + ',' + d.y + ')';
+    //     })
+
+    // node.append('circle')
+    //     .text(function(d) {
+    //         return d.data.Item;
+    //     })
+    //     .attr('r', function(d) {
+    //         return d.r;
+    //     })
+    //     .attr('fill', function(d) {
+    //         return
+    // });
 
     //Call draw function
 });
+})
+
